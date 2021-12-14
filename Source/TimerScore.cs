12bf71c7f526ccb783284b9a45ed3e31cd62ac1b
mansam3 <93827601+mansam3@users.noreply.github.com>
2021-12-14 22:50:28 +0900

    Add files via upload

---
+++ Source/TimerScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScore : MonoBehaviour
+{
+    GameObject timer;
+    float time =0;
+    void Start()
+    {
+        this.timer = GameObject.Find("TimeScore");        
+    }
+
+   
+    void Update()
+    {
+        this.time += Time.deltaTime;
+        this.timer.GetComponent<Text>().text = this.time.ToString("F1")+("초");
+    }
+}
